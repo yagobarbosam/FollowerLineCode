a6f8dbd6295fc3722522c6fecafb5e443321873c
yagobarbosam <yago.barbosam@hotmail.com>
2016-10-08 13:29:56 -0300

    Update ReflectanceSensors.h

--- FollowerLineCode/ReflectanceSensors.h
+++ FollowerLineCode/ReflectanceSensors.h
@@ -2,20 +2,31 @@
  * File:   ReflectanceSensors.h
  * Author: Yago
  *
- * Created on 4 de Junho de 2016, 10:38
+ * Created on 8 de Outrubro de 2016, 10:38
  */
 
 #ifndef REFLECTANCESENSORS_H
 #define	REFLECTANCESENSORS_H
 
-typedef enum {
-    WHITE, BLACK
-} color;
+//----------------------------
+//   Auxiliary Constants  
+//----------------------------
+
+typedef enum {WHITE, BLACK} color;
+
+//----------------------------
+//     Function prototype 
+//----------------------------
+
+void reflectanceSensors_init(void);
 
 unsigned int return_distanceLine(void);
 
 void calibrates_sensors(void);
+
 void read_sensorns(color, unsigned char);
+
+//----------------------------
 
 #endif	/* REFLECTANCESENSORS_H */
 
